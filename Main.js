4b10ff617623b10ed8d49a171b5dc29e362eeed6
EmmaDahlqvist <emma.dahlqvist@elev.ga.ntig.se>
2022-09-13 16:13:29 +0200

    finished

--- Main.js
+++ Main.js
@@ -1,25 +1,32 @@
 //valt nummer
-let number = 30;
+let correctNumber = 30;
 
 //input
 var ps = require("prompt-sync");
 var prompt = ps();
 
-//intro
 console.log("Guess the number between 0 - 100");
 
-//10 gissningar
-for(let i = 10; i >= 0; i--){
+//10 gissningar, i representerar liv kvar
+for(let i = 9; i >= 0; i--){
     //input
     var input = prompt();
 
     //rätt gissning
-    if(input == number){
-        console.log("You guessed " + number + " correctly!");
+    if(input == correctNumber){
+        console.log("You guessed " + correctNumber + " correctly!");
         break; //bryt loopen
-        //fram tills 1 liv kvar
+
+        //fel - fram tills 1 liv kvar
     } else if (i > 0){
-        console.log("Wrong. Lives left: " + i);
+        if(input > correctNumber){
+            console.log("Try a lower number. Lives left: " + i); //för högt
+        } else if(input < correctNumber){
+            console.log("Try a higher number. Lives left: " + i); //för lågt
+        } else {
+            console.log("Wrong. Lives left: " + i);
+        }
+
         //0 liv kvar
     } else {
         console.log("Game over! Better luck next time")
